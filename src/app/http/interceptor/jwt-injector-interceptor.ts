dad1f12cf3c1026b5bc06d96ea1bb83d4ee0f236
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-03-10 23:26:23 -0400

    previous jwt interceptor wasnt saved before commiting. commint after saving th impl

--- src/app/http/interceptor/jwt-injector-interceptor.ts
+++ src/app/http/interceptor/jwt-injector-interceptor.ts
@@ -1,5 +1,21 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthStore } from '../../auth/auth-store';
+
+
 
 export const jwtInjectorInterceptor: HttpInterceptorFn = (req, next) => {
+  const authStore = inject(AuthStore);
+
+  if (authStore.isAuthentificated()) {
+    const token = authStore.token;
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    console.log("token injected");
+  }
+
   return next(req);
 };
