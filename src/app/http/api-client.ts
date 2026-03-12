b93c71cc380ee5332975d254812b2e532706ad07
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-03-12 15:13:47 -0400

    modify api  to handle errors by logging them and wrapping them

--- src/app/http/api-client.ts
+++ src/app/http/api-client.ts
@@ -1,6 +1,10 @@
-import { HttpClient } from "@angular/common/http"
+import { HttpClient, HttpErrorResponse } from "@angular/common/http"
 import { inject, Injectable } from "@angular/core"
-import { Observable } from "rxjs"
+import { catchError, Observable, throwError } from "rxjs"
+
+export interface ApiErrorResponse {
+  error: string;
+}
 
 @Injectable({
     providedIn: 'root'
@@ -11,20 +15,55 @@ export class ApiClient {
     readonly #httpClient = inject(HttpClient)
 
     get<T>(path: string): Observable<T>  {
-        return this.#httpClient.get<T>(`${this.#BASE_URL}/${path}`)
+        return this.#httpClient.get<T>(`${this.#BASE_URL}${path}`).pipe(
+            catchError(this.handleError)
+        );
     }
 
     post<T>(path: string, body: unknown): Observable<T> {
-        return this.#httpClient.post<T>(`${this.#BASE_URL}/${path}`, body)
+        return this.#httpClient.post<T>(`${this.#BASE_URL}${path}`, body).pipe(
+            catchError(this.handleError)
+        );
     }
 
     patch<T>(path: string, body: unknown): Observable<T> {
-        return this.#httpClient.patch<T>(`${this.#BASE_URL}/${path}`, body)
+        return this.#httpClient.patch<T>(`${this.#BASE_URL}${path}`, body).pipe(
+            catchError(this.handleError)
+        );
     }
 
     delete<T>(path: string): Observable<T> {
-        return this.#httpClient.delete<T>(`${this.#BASE_URL}/${path}`)
+        return this.#httpClient.delete<T>(`${this.#BASE_URL}${path}`).pipe(
+            catchError(this.handleError)
+        );
     }
 
+    private handleError(error: HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.error('An error occurred:', error.error.message);
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong.
+            console.error(
+                `Backend returned code ${error.status}, ` +
+                `body was: ${JSON.stringify(error.error)}`);
+        }
+        // Return an observable with a user-facing error message.
+        return throwError(() => new ApiError(error.error, error.status));
+    }
+}
+
+export class ApiError extends Error implements ApiErrorResponse {
+  error: string;
+  status: number;
+  suggested_username?: string;
+
+
+  constructor(resp: ApiErrorResponse, status: number) {
+    super(resp.error);
+    this.error = resp.error;
+    this.status = status;
+  }
 
 }
