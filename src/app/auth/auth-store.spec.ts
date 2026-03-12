a6b5fc8cff3d1ed846ade2314a4f96559bdb6e59
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-03-12 15:27:41 -0400

    remove all test files (no test used)

--- src/app/auth/auth-store.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuthStore } from './auth-store';
-
-describe('AuthStore', () => {
-  it('should create an instance', () => {
-    expect(new AuthStore()).toBeTruthy();
-  });
-});
--- src/app/http/api-client.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ApiClient } from './api-client';
-
-describe('ApiClient', () => {
-  it('should create an instance', () => {
-    expect(new ApiClient()).toBeTruthy();
-  });
-});
