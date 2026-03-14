8c6f9228ad7bfd8b3f7006394e8444a395fa47c1
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-03-14 12:53:21 -0400

    generate job search form page component

---
+++ src/app/pages/job-search-form/job-search-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-search-form',
+  imports: [],
+  templateUrl: './job-search-form.html',
+  styleUrl: './job-search-form.scss',
+})
+export class JobSearchForm {}
