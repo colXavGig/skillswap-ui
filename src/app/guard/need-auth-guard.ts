5da32d7e98277c1aa89ee0f70409ba4b0f275ab4
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-03-16 00:19:10 -0400

    modify guard to redirect to login page

--- src/app/guard/need-auth-guard.ts
+++ src/app/guard/need-auth-guard.ts
@@ -1,9 +1,20 @@
 import { CanActivateFn } from '@angular/router';
 import { AuthStore } from '../auth/auth-store';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const needAuthGuard: CanActivateFn = (route, state) => {
   const authStore = inject(AuthStore);
+  const router = inject(Router);
 
-  return authStore.isAuthentificated();
+  if (authStore.isAuthentificated()) {
+    return true;
+  }
+
+  return router.createUrlTree(['/login'], {
+    queryParams: {
+      returnUrl: state.url,
+      error: 'Please log in to continue.',
+    },
+  });
 };
