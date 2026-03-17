4b50e591c33585005cfc45372410fb0d98e68ae2
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-03-16 21:57:23 -0400

    add endpoints for the newly implemented pages
    - job creation
    - list jobs
    - job details
    - list proposal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,10 @@ import { Profile } from './pages/profile/profile';
 import { needAuthGuard } from './guard/need-auth-guard';
 import { JobSearchResult } from './pages/job-search-result/job-search-result';
 import { JobSearchForm } from './pages/job-search-form/job-search-form';
+import { JobCreateComponent } from './pages/job/job-create/job-create';
+import { JobDetailComponent } from './pages/job/job-detail/job-detail';
+import { MyPostingsComponent } from './pages/job/my-postings/my-postings';
+import { MyBidsComponent } from './pages/proposals/my-bids/my-bids';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -14,6 +18,10 @@ export const routes: Routes = [
   { path: 'profile/:username', component: Profile },
   { path: 'jobs/search-result', component: JobSearchResult },
   { path: 'jobs/search', component: JobSearchForm },
+  { path: 'jobs/create', component: JobCreateComponent, canActivate: [needAuthGuard] },
+  { path: 'jobs/my-postings', component: MyPostingsComponent, canActivate: [needAuthGuard] },
+  { path: 'jobs/:id', component: JobDetailComponent, canActivate: [needAuthGuard] },
+  { path: 'proposals/my-bids', component: MyBidsComponent, canActivate: [needAuthGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
