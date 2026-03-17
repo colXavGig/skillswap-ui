5e166126daafceaa44239db797daf64e47921b87
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-03-16 21:59:05 -0400

    add navbar that changes depending  on the authentification status

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthStore } from './auth/auth-store';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  readonly #authStore = inject(AuthStore);
+  readonly #router = inject(Router);
   protected readonly title = signal('skillswap-ui');
+
+  get isAuthenticated() {
+    return this.#authStore.isAuthentificated();
+  }
+
+  logout() {
+    this.#authStore.clearSession();
+    this.#router.navigate(['/login']);
+  }
 }
